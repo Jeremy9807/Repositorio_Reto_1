c5a9a28925e6600cd9762a44e59e9ccccbb5d90b
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-04 11:13:24 -0500

    Configuracion endpoint Listar Productos

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-app.use(require('./Rutas/rutas'));
+app.use('/admin/producto',require('./Rutas/listar_productos')); //Endpoint numero 1
 
 // Iniciando server
 app.listen(app.get('port'), () => {
