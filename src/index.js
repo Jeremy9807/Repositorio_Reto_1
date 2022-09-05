8491647e0e077221540e5a6bc41362c61ed73923
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-05 15:50:17 -0500

    Actualizacion de informacion y cambio de nombre de rama

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-app.use('/admin/producto', require('./Rutas/actualizar_producto'));//Endpoiont numero 4. Actualizar producto
+app.use('/admin/producto', require('./Rutas/actualizar_producto'));//Endpoiont numero 4. Actualizar producto Web
 
 // Iniciando server
 app.listen(app.get('port'), () => {
