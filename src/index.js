3030d824af90addbac6f56088bb051debfe5403e
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-05 16:02:37 -0500

    Actualizacion de informacion

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-app.use('/producto', require('./Rutas/consultar_producto_movil'));
+app.use('/producto', require('./Rutas/consultar_producto_movil'));//Endpoint numero 8. Consultar producto movil
 
 // Iniciando server
 app.listen(app.get('port'), () => {
