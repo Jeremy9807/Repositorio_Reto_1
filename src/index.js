fb01d701caeda6c0440fd8325ee2c80ec67bd0f1
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-05 15:44:30 -0500

    Actualizacion de informacion y nombre de rama

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-app.use('/admin/producto/',require('./Rutas/consultar_producto')); //Endpoint Numero 2, consultar producto especifico
+app.use('/admin/producto/',require('./Rutas/consultar_producto')); //Endpoint Numero 2, consultar producto Web
 
 // Iniciando server
 app.listen(app.get('port'), () => {
