c4217d889b151adbdc772fef914bdd7bac503da7
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-05 17:08:34 -0500

    Merge de la rama Consutlar_Producto_Web

--- src/index.js
+++ src/index.js
@@ -11,14 +11,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-<<<<<<< HEAD
-
 app.use('/admin/producto', require('./Rutas/actualizar_producto'));//Endpoiont numero 4. Actualizar producto Web
 app.use('/admin/ordenes',require('./Rutas/consultar_ordenes'));//Endpoint 6. Consultar ordenes de compra Web
-
-=======
 app.use('/admin/producto/',require('./Rutas/consultar_producto')); //Endpoint Numero 2, consultar producto Web
->>>>>>> Consultar_Producto_Web
 
 // Iniciando server
 app.listen(app.get('port'), () => {
