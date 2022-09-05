8cbfeefea64efb6b79868e43bef3a98583d808a1
Jeremy9807 <jeremyjbo12345@hotmail.com>
2022-09-05 01:44:12 -0500

    Configuracion endpoint Realizar compra

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 //Rutas
-app.use(require('./Rutas/rutas'));
+app.use('/comprar', require('./Rutas/realizar_compra'));//Endpoint numero 10, Realizar Compra
 
 // Iniciando server
 app.listen(app.get('port'), () => {
